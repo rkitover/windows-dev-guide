19d064d1b05840e4032696b17cb1f43886589c41
Rafael Kitover <rkitover@gmail.com>
2022-11-26 01:58:20 +0000

    Remove terminal from choco package list
    Remove microsoft-windows-terminal from the list of choco packages to
    install as it is now managed by Windows 11.
    
    Leave a commented install command for Windows 10 users.
    
    Also leave a commented command for users who had previously installed
    the choco package and then upgraded to Windows 11 to remove the package
    from the list without uninstalling it.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- install.ps1
+++ install.ps1
@@ -5,6 +5,10 @@ choco install -y visualstudio2022community --params '--locale en-US'
 choco install -y visualstudio2022-workload-nativedesktop
 choco install -y vim --params '/NoDesktopShortcuts'
 choco install -y 7zip NTop.Portable StrawberryPerl bzip2 cmake.portable dejavufonts diffutils dos2unix file gawk git gpg4win grep gzip hackfont less make microsoft-windows-terminal neovim netcat nodejs notepadplusplus powershell-core python ripgrep sed sshfs unzip xxd zip
+## Only run this on Windows 10 or older, this package is managed by Windows 11.
+#choco install -y microsoft-windows-terminal
+## If you had previously installed it and are now using Windows 11, run:
+#choco uninstall microsoft-windows-terminal -n --skipautouninstaller
 stop-service ssh-agent
 sc.exe delete ssh-agent
 choco install -y openssh --prerelease --force --params '/SSHServerFeature /SSHAgentFeature /PathSpecsToProbeForShellEXEString:$env:programfiles\PowerShell\*\pwsh.exe'
