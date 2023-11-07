40696fa633492207b438c94a61d1e806a8adbfd7
Rafael Kitover <rkitover@gmail.com>
2023-11-07 16:11:50 +0000

    Make ports task auto-restart on death
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- ports-task.ps1
+++ ports-task.ps1
@@ -8,11 +8,14 @@ $action  = new-scheduledtaskaction `
     -execute 'ssh' `
     -argument '-NT server-ports'
 
+$settings = new-scheduledtasksettingsset -restartcount:1000 -restartinterval (new-timespan -minutes 1)
+
 $password = (get-credential $env:username).getnetworkcredential().password
 
 register-scheduledtask -force `
     -taskname $taskname `
     -trigger $trigger -action $action `
+    -settings $settings `
     -user $env:username `
     -password $password `
     -ea stop | out-null
