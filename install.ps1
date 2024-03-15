008ed49c7e01e3a2f7aa720e8115a21063787283
Rafael Kitover <rkitover@gmail.com>
2024-03-15 02:17:04 -0700

    Admin install script fixes
    Use powershell.exe and the full path to pwsh.exe because it is not yet
    in $env:PATH.
    
    Restart the ssh services instead of starting them, in case they are
    already running to reload the configuration.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- install.ps1
+++ install.ps1
@@ -12,15 +12,15 @@ iwr https://aka.ms/vs/17/release/vs_community.exe -outfile vs_community.exe
 
 ./vs_community.exe --passive --add 'Microsoft.VisualStudio.Workload.NativeDesktop;includeRecommended;includeOptional'
 
-start-process pwsh '-noprofile', '-windowstyle', 'hidden', `
+start-process powershell '-noprofile', '-windowstyle', 'hidden', `
     '-command', "while (test-path $pwd/vs_community.exe) { sleep 5; ri -fo $pwd/vs_community.exe }"
 
-new-itemproperty -path "HKLM:\SOFTWARE\OpenSSH" -name DefaultShell -value (get-command pwsh).source -propertytype string -force > $null
+new-itemproperty -path "HKLM:\SOFTWARE\OpenSSH" -name DefaultShell -value '/Program Files/PowerShell/7/pwsh.exe' -propertytype string -force > $null
 
 (gc /programdata/ssh/sshd_config) | %{ $_ -replace '^([^#].*administrators.*)','#$1' } | set-content /programdata/ssh/sshd_config
 
 set-service sshd -startuptype automatic
 set-service ssh-agent -startuptype automatic
 
-start-service sshd
-start-service ssh-agent
+restart-service sshd
+restart-service ssh-agent
