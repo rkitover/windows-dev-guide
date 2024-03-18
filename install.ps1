81d95438e4995b449b98dc05f435cd3b966866c3
Rafael Kitover <rkitover@gmail.com>
2024-03-18 21:09:52 +0000

    Remove regular vim from install script
    Since neovim is so much better on Windows, and that is what the profile
    uses, stop installing regular vim in the install script.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- install.ps1
+++ install.ps1
@@ -2,9 +2,10 @@
 
 set-service beep -startuptype disabled
 
-'Microsoft.VisualStudio.2022.Community','vim.vim','7zip.7zip','gsass1.NTop','Git.Git','GnuPG.GnuPG',`
-'SourceFoundry.HackFonts','Neovim.Neovim','OpenJS.NodeJS','Notepad++.Notepad++','Microsoft.Powershell',`
-'Python.Python.3.13','SSHFS-Win.SSHFS-Win','Microsoft.OpenSSH.Beta','Microsoft.WindowsTerminal' | %{
+'Microsoft.VisualStudio.2022.Community','7zip.7zip','gsass1.NTop','Git.Git',`
+'GnuPG.GnuPG','SourceFoundry.HackFonts','Neovim.Neovim','OpenJS.NodeJS',`
+'Notepad++.Notepad++','Microsoft.Powershell',`,'Python.Python.3.13',`
+'SSHFS-Win.SSHFS-Win','Microsoft.OpenSSH.Beta','Microsoft.WindowsTerminal' | %{
 	winget install $_
 }
 
