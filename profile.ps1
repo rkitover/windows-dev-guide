66b64489ebc6f067038e5493b6eec8d88a15109c
Rafael Kitover <rkitover@gmail.com>
2024-08-05 22:32:18 +0000

    profile: add comment for reset function
    Followup on 241e47b (profile: add reset function, 2024-08-05), add
    comment explaining what the `reset` function for Windows tries to reset
    the terminal to a sane state.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- profile.ps1
+++ profile.ps1
@@ -617,6 +617,8 @@ if ($iswindows) {
         }
     }
 
+    # Tries to reset the terminal to a sane state, similar to the Linux reset
+    # binary from ncurses-utils.
     function global:reset {
         clear-host
         [char]27 + "[!p"
