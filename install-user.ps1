a01b186208e273f697e96d858bca6eecec72739f
Rafael Kitover <rkitover@gmail.com>
2024-08-31 15:44:22 +0000

    install-user.ps1: add busybox to Scoop packages
    Add the "busybox" package to the list of packages to install from Scoop
    in the user install script, putting it first in the list so that its
    shims for "grep", "awk", "unzip", etc. are overwritten by the GNU and
    other utilities.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- install-user.ps1
+++ install-user.ps1
@@ -4,7 +4,8 @@ if (-not (test-path ~/scoop)) {
     iwr get.scoop.sh | iex
 }
 
-~/scoop/shims/scoop.cmd install bzip2 diffutils dos2unix file gawk grep gzip less make mingw perl ripgrep sed zip unzip
+# busybox must be first in the installation order.
+~/scoop/shims/scoop.cmd install busybox bzip2 diffutils dos2unix file gawk grep gzip less make mingw perl ripgrep sed zip unzip
 ~/scoop/shims/scoop.cmd bucket add nerd-fonts
 ~/scoop/shims/scoop.cmd install DejaVuSansMono-NF
 
