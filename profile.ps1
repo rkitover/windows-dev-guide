rkitover/windows-dev-guide

commit 5709bff4e1d39c92aeec8b1d1821ad15e640da39

profile: remove spaces in path parts in shortpath
For the profile `shortpath` function, convert path parts with spaces to
short DOS paths.

Signed-off-by: Rafael Kitover <rkitover@gmail.com>

profile.ps1
@@ -103,11 +103,43 @@ function backslashes_to_forward($str) {
     $str -replace '\\','/'
 }
 
+function global:remove_path_spaces($path) {
+    if (-not $path) { $path = $($input) }
+
+    if (-not $iswindows) { return $path }
+
+    if (-not $path) { return $path }
+
+    $parts = while ($path -notmatch '^\w+:[\\/]$') {
+        $leaf = split-path -leaf   $path
+        $path = split-path -parent $path
+
+        $fs = new-object -comobject scripting.filesystemobject
+
+        if ($leaf -match ' ') {
+            $leaf = if ((gi "${path}/$leaf").psiscontainer) {
+                split-path -leaf $fs.getfolder("${path}/$leaf").shortname
+            }
+            else {
+                split-path -leaf $fs.getfile("${path}/$leaf").shortname
+            }
+        }
+        
+        $leaf.tolower()
+    }
+
+    if ($parts) { [array]::reverse($parts) }
+
+    $path = $path -replace '[\\/]+', ''
+
+    $path + '/' + ($parts -join '/')
+}
+
 function global:shortpath($str) {
     if (-not $str) { $str = $($input) }
 
     $str | resolve-path -ea ignore | % path `
-        | trim_curdrive | backslashes_to_forward
+        | remove_path_spaces | trim_curdrive | backslashes_to_forward
 }
 
 function global:realpath($str) {
@@ -537,10 +569,7 @@ if ($iswindows) {
             set-alias nvim -value $vim -scope global
         }
 
-        # Remove spaces from path if possible, because this breaks UNIX ports.
-        $env:EDITOR = realpath $vim `
-            | %{ $_ -replace '/Program Files/','/progra~1/' } `
-            | %{ $_ -replace '/Program Files (x86)/','/progra~2/' } `
+        $env:EDITOR = shortpath $vim
     }
 }
 else {

commit 1e9c90e6133b0d40614acacad60dcdfbfae9afaf

profile: fix setting $env:EDITOR to nvim
Fix regression introduced in 5709bff (profile: remove spaces in path
parts in shortpath, 2025-01-03) which changed setting `$env:EDITOR` to
`nvim` not prefixed by the drive, which does not work in the Msys2
runtime which Git for Windows uses.

Change `realpath` to work the same as `shortpath` but keeping the drive,
and use it to set `$env:EDITOR` to nvim.

Signed-off-by: Rafael Kitover <rkitover@gmail.com>

profile.ps1
@@ -145,7 +145,8 @@ function global:shortpath($str) {
 function global:realpath($str) {
     if (-not $str) { $str = $($input) }
 
-    $str | resolve-path -ea ignore | % path | backslashes_to_forward
+    $str | resolve-path -ea ignore | % path `
+        | remove_path_spaces | backslashes_to_forward
 }
 
 function global:syspath($str) {
@@ -569,7 +570,7 @@ if ($iswindows) {
             set-alias nvim -value $vim -scope global
         }
 
-        $env:EDITOR = shortpath $vim
+        $env:EDITOR = realpath $vim
     }
 }
 else {